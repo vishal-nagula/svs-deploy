ea4685f659c3436a67362b63c6f72ef8098a76d2
Vishal Nagula <nagula.vishal7@gmail.com>
2026-01-07 13:29:35 +0530

    Updatae certs

--- generate-certs.sh
+++ generate-certs.sh
@@ -20,18 +20,18 @@ major_version=$(openssl version | awk '{print $2}' | cut -d. -f1)
 
 if [ "$major_version" == "1" ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout "$CERT_DIR/server.key" \
-    -out "$CERT_DIR/server.crt" \
+    -keyout "$CERT_DIR/privkey.pem" \
+    -out "$CERT_DIR/fullchain.pem" \
     -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
     echo "Certificates generated successfully (OpenSSL 1.x)."
 else 
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout "$CERT_DIR/server.key" \
-    -out "$CERT_DIR/server.crt" \
+    -keyout "$CERT_DIR/privkey.pem" \
+    -out "$CERT_DIR/fullchain.pem" \
     -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost" \
     -addext "subjectAltName=DNS:localhost"
     echo "Certificates generated successfully (OpenSSL 3.x with SAN)."
 fi
 
-chmod 644 "$CERT_DIR/server.crt"
-chmod 600 "$CERT_DIR/server.key"
+chmod 644 "$CERT_DIR/fullchain.pem"
+chmod 600 "$CERT_DIR/privkey.pem"
